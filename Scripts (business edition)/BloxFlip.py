9f6a0c111fd3a822fec51ecf0d60a448ceade40e
thwartedbrute <115373618+ewwalkdfljalkgjleas@users.noreply.github.com>
2023-03-27 02:28:27 -0400

    Add files via upload

--- Scripts (business edition)/BloxFlip.py
+++ Scripts (business edition)/BloxFlip.py
@@ -15,19 +15,19 @@ try:
   os.system('cls')
 except:
   os.system('clear')
-import pyautogui, cloudscraper ; from termcolor import cprint
+import pyautogui, cloudscraper; from termcolor import cprint
 os.system(f'title RBLXRain ^')
 cprint('██████╗░░░██╗██╗██████╗░██╗░░██╗░██████╗░█████╗░██████╗░░░███╗░░██████╗░████████╗░██████╗\n██╔══██╗░██╔╝██║██╔══██╗██║░██╔╝██╔════╝██╔══██╗██╔══██╗░████║░░██╔══██╗╚══██╔══╝██╔════╝\n██║░░██║██╔╝░██║██████╔╝█████═╝░╚█████╗░██║░░╚═╝██████╔╝██╔██║░░██████╔╝░░░██║░░░╚█████╗░\n██║░░██║███████║██╔══██╗██╔═██╗░░╚═══██╗██║░░██╗██╔══██╗╚═╝██║░░██╔═══╝░░░░██║░░░░╚═══██╗\n██████╔╝╚════██║██║░░██║██║░╚██╗██████╔╝╚█████╔╝██║░░██║███████╗██║░░░░░░░░██║░░░██████╔╝\n╚═════╝░░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░░░░░░░╚═╝░░░╚═════╝░', 'cyan')
 cprint("[CREDITS:] AutoRain made by Thwartedbrute#8188","cyan")
 cprint("AutoRain Enabled! Press Ctrl + C to exit. Timestamp: " + time.strftime("%m/%d/%Y %H:%M:%S", time.localtime(int(time.time()))) + "\n","magenta")
 while True:
   try:
-    r = cloudscraper.create_scraper().get('http://rest-bf.blox.land/chat/history').json()
-    check = r['rain']
+    check = cloudscraper.create_scraper().get('http://rest-bf.blox.land/chat/history').json()['rain']
     if check['active'] == True:
+      print(check)
       if pyautogui.locateCenterOnScreen('Images/BloxFlip.png', confidence = 0.9):
         time.sleep(2)
-        pyautogui.moveTo(pyautogui.locateCenterOnScreen('Images/BloxFlip.png', confidence = 0.9))
+        pyautogui.moveTo(pyautogui.locateOnScreen('Images/BloxFlip.png', confidence = 0.9))
         time.sleep(0.5)
         pyautogui.click()
         time.sleep(2)
@@ -51,5 +51,6 @@ while True:
         time.sleep(10)
     elif check['active'] == False:
       time.sleep(5)
-  except:
+  except Exception as e:
+    print(e)
     time.sleep(5)
